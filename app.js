a158354c8472569a437277d6dd8ba52dce146e7b
1 <raamatkeijo@gmail.com>
2020-02-19 15:53:57 +0200

    Added movie descriptions in second 'div'

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ fetch('https://ghibliapi.herokuapp.com/films')
     const moviesContainer = document.getElementById('movies')
     movies.forEach((movie) => {
       let movieContainer = document.createElement('div')
-      movieContainer.innerHTML = movie.title
+      movieContainer.classList.add('movie')
+      movieContainer.innerHTML = '<div class="title">' + '<h1>'+ movie.title + '</h1>'+ '</div>' + '<div class="description">' + movie.description + '</div>'
       moviesContainer.append(movieContainer)
       console.log(movie.title);
     });
